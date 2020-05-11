0bcd84ad4df0192a314a1534889f2afb9f6d90a1
Artur <54365622+bunge12@users.noreply.github.com>
2020-05-11 11:24:43 -0700

    Changed render method

--- server.js
+++ server.js
@@ -2,24 +2,19 @@ const Express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const App = Express();
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
 
-App.set("view engine", "ejs");
 App.use(morgan("dev"));
 App.use(Express.static("semantic/dist"));
 App.use(cookieParser());
 App.use(bodyParser.urlencoded({ extended: true }));
-
+const publicPath = path.join(__dirname, "../public");
+// App.use(Express.static(publicPath));
+App.use(Express.static("public"));
+///
 const { generateRoom } = require("./scripts/generateRoom");
-
-App.get("/", (req, res) => {
-  res.render("index");
-});
 
 App.get("/start", (req, res) => {
   let name = null;
@@ -51,8 +46,8 @@ App.post("/game/:id", (req, res) => {
   res.send(result);
 });
 
-App.listen(port, () => {
+App.listen(8000, () => {
   console.log(
-    `Express seems to be listening on port ${port} so that's pretty good 👍`
+    `Express seems to be listening on port 8000 so that's pretty good 👍`
   );
 });
